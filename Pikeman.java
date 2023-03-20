e717212893ae1f967b8e6736d1247ad5e30081ef
Oleg <tmr777@mail.ru>
2023-03-20 19:14:21 +0300

    Исходный файл Pikeman.java

---
+++ Pikeman.java
@@ -0,0 +1,15 @@
+public class Pikeman extends Shooter {
+    
+    protected int pike= 1;
+
+    public Pikeman(){
+        super(10, 7, 10, 0);
+        
+     }
+ 
+     public String getInfo() {
+         return String.format("Копейщик: жизнь - %.1f  скорость - %d  Атаки - %d,  Пика - %d",
+                 this.hp, this.speed, this.damage,  this.pike);
+     }
+ 
+}
