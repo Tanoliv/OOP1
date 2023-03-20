91002e12412e03be062edccd7bdc6411fff80caa
Oleg <tmr777@mail.ru>
2023-03-20 19:10:59 +0300

    Исходный файл Monk.java

---
+++ Monk.java
@@ -0,0 +1,13 @@
+public class Monk extends Unit {
+    protected int medicines= 100;
+
+    public Monk(){
+        super(10, 3, 10);
+        
+     }
+ 
+     public String getInfo() {
+         return String.format("Монах: жизнь - %.1f  скорость - %d, лекарства - %d",
+                 this.hp, this.speed,   this.medicines);
+     }
+}
