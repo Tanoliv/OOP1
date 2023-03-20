a74653d41e6d223d4b34331818ef42afd7c1e676
Oleg <tmr777@mail.ru>
2023-03-20 19:17:29 +0300

    Исходный файл Shooter.java

---
+++ Shooter.java
@@ -0,0 +1,12 @@
+public class Shooter extends Unit {
+   
+    protected int arrows;
+
+    public Shooter(float hp, int speed, int damage, int arrows){
+        super( hp, speed, damage);
+        this.arrows= arrows;
+    }
+
+  
+
+}
