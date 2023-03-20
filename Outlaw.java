c0af9270b62d6c7a1e3bbb67997b48f702b0f718
Oleg <tmr777@mail.ru>
2023-03-20 19:12:14 +0300

    Исходный файл Outlaw.java

---
+++ Outlaw.java
@@ -0,0 +1,14 @@
+public class Outlaw extends Shooter {
+    protected int blockhead= 1;
+    protected int knife= 1;
+
+    public Outlaw(){
+        super(10, 7, 10, 0);
+        
+     }
+ 
+     public String getInfo() {
+         return String.format("Разбойник: жизнь - %.1f  скорость - %d  Атаки - %d,  Дубина - %d, Нож - %d",
+                 this.hp, this.speed, this.damage,  this.blockhead, this.knife);
+     }
+}
