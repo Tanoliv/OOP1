fd5a3f58ad133a19c0fafcaadafa294af4394d0d
Tanoliv <114357687+Tanoliv@users.noreply.github.com>
2023-03-21 13:35:23 +0300

    Add files via upload

---
+++ DzSem6Zadanie41/Program.cs
@@ -0,0 +1,75 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел.
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 4       
+
+
+/*Console.Write("Введите количество элементов массива:\t");
+int size = int.Parse(Console.ReadLine()!);
+int[] mass = new int[size];
+
+Console.WriteLine("Вывод массива:");
+
+for(int i =0; i < mass.Length; i++)
+{
+    Console.Write($"Введите элемент массива с индексом {i}:\t");
+    mass[i] = int.Parse(Console.ReadLine()!);
+}
+
+ for(int i = 0; i < mass.Length; i++)
+ {
+    //Console.WriteLine($"\t{mass[i]}");
+     
+    Console.Write(String.Join(" ", mass[i]));
+    Console.WriteLine();
+ }
+ //Console.ReadLine();
+
+ int result =0;
+ for(int i = 0; i < mass.Length; i++)
+ {
+    if(mass[i] > 0)
+    { 
+        result++;
+        
+    }   
+     
+ }
+
+ Console.WriteLine($"Количество четных элементов в массиве = {result}");*/
+
+
+
+Console.Write("Введите количество элементов массива:\t");
+int size = int.Parse(Console.ReadLine()!);
+int[] mass = new int[size];
+
+Console.WriteLine("Вывод массива:");
+
+for(int i =0; i < mass.Length; i++)
+{
+    Console.Write($"Введите элемент массива с индексом {i}:\t");
+    mass[i] = int.Parse(Console.ReadLine()!);
+}
+
+ for(int i = 0; i < mass.Length; i++)
+ {
+    Console.Write(String.Join(" ", mass[i]));
+    Console.WriteLine();
+ }
+
+ Console.WriteLine($"Количество элементов в массиве  > 0 = {Method()}");
+
+
+int Method()
+{
+     int result =0;
+    for(int i = 0; i < mass.Length; i++)
+    {
+        if(mass[i] > 0)
+        { 
+           result++;        
+        }        
+    }
+    return result;
+}
